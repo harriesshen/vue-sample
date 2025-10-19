79c076d90831b66a69953a3b883ad252e9c9280f
harries4206 <harries4206@gmail.com>
2025-10-19 23:22:07 +0800

    refactor(i18n): i18n initialization 1. i18n initialization 2. i18n structure

--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,8 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-import { createI18n } from 'vue-i18n'
-import messages from './locales'
+import { i18n } from './locales'
 const app = createApp(App)
-
-console.log('locale messages', messages)
-const i18n = createI18n({
-  legacy: false,
-  locale: 'zhTW', // 設定預設語系
-  fallbackLocale: 'en', // 設定備用語系
-  messages,
-})
 
 app.use(createPinia())
 app.use(router)
